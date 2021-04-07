07ea6cc4bdb48be94e2495cf5bca5f59593b4be7
nikmax <anton@sander.kz>
2021-04-07 17:38:39 +0200

    now with objects

---
+++ js/classes.js
@@ -0,0 +1,131 @@
+class Character {
+  constructor(x,y,canvas,scale) {
+    this.characterSpeed = 5;
+    this.gameSpeed = 0;
+    this.longidle = 1000 * 20;
+    this.canvas = canvas;
+  	this.x = x;
+  	this.y = y;
+    this.isMoving = false;
+  	this.scale = scale;
+  	this.animateIndex = 1;
+    this.images = new Array();   // array of array of state images
+    this.state;
+    this.newstate = '';
+    this.timestate = new Date().getTime();
+    this.interval = 0;
+    setInterval(this.animate.bind(this),20);  // 10
+  }
+  addState(state,path,pics) {
+  	this.images[state] = [path,pics];
+    this.state = state;
+  }
+  getState(){
+    //
+  	return this.state;
+  }
+  setState(state){
+  	if(this.checkState(state) ){
+  		this.newstate = state;
+  	}
+  }
+  checkState(state){
+  	if(this.images[state] === undefined) return false;
+  	return true;
+  }
+  move(){
+  	//if(!this.isMoving){
+  		this.setState('Walk');
+	  	this.isMoving = true;
+  	//}
+  	//this.speed = speed;
+  }
+  halt(){
+  	this.isMoving = false;
+  	this.setState('Idle');
+  }
+  setError (err){
+    //
+  	this.error = err;
+  }
+  animate(){
+    this.interval++;
+		if(this.interval > 10){
+      this.animateIndex++;
+      this.interval = 0;
+    }
+    if(this.animateIndex > this.images[this.state][1]){
+      this.animateIndex = 1;
+    }
+    this.checkForLongIdle();
+    if(this.newstate != this.state){
+      this.state = this.newstate;
+      this.animateIndex = 1;
+      this.timestate = new Date().getTime();
+    }
+    if(this.gameSpeed > 0){
+      if(this.isMoving) this.x += this.characterSpeed - this.gameSpeed;
+      else this.x -= this.gameSpeed;
+   }
+  }
+  checkForLongIdle(){
+    if(this.checkState('Long_Idle')){
+      if(this.state == 'Idle'){
+        let time = new Date().getTime();
+        if ((time - this.timestate) >= this.longidle) this.setState('Long_Idle');
+      }
+    }
+  }
+  drawImage() {
+    let ctx = this.canvas.getContext("2d");
+    let img = new Image();
+    img.src = this.images[this.state][0] + "/" + this.animateIndex + ".png"; 
+    //checkForJumping();
+    if (img.complete) ctx.drawImage(img,this.x,this.y,img.width*this.scale,img.height*this.scale);
+  }
+}
+/**
+ * describe a class of background images
+ * initial draw from 0,0 an scale to canvas size
+ * @class BgImage - Constructor
+ * @param {string} path - The path to the two images (1.png and 2.png) for continue move
+ * @param {element} canvas
+ * @param {float} initialspeed
+ * @method drawImage - draw a backgroud image in a canvas
+ * @method animate - private method for move a background with a speed
+ * @method setSpeed - Set the new speed from right for backround (negotate from left)
+ */
+class BgImage {
+  constructor(path,canvas,initialspeed) {
+    this.x = 0;
+    this.path = path;
+    this.canvas = canvas;
+    this.speed = initialspeed;
+    this.gameSpeed = 0;
+    setInterval(this.animateBg.bind(this),20);
+  }
+  drawImage() {
+    let bg0 = new Image();
+    let bg1 = new Image();
+    let bg2 = new Image();
+    let ctx = this.canvas.getContext("2d");
+    let width = this.canvas.width;  // for short code
+    let dx = parseInt( this.x / width);
+    let ds = this.x % width;
+
+    if (dx%2 == 0){
+      bg0.src = this.path + "/2.png"; bg1.src = this.path + "/1.png"; bg2.src = this.path + "/2.png";
+    }else{
+      bg0.src = this.path + "/1.png"; bg1.src = this.path + "/2.png"; bg2.src = this.path + "/1.png";
+    }
+    if(bg0.complete) ctx.drawImage(bg0,ds - width,0,width,bg0.height * width / bg0.width);
+    if(bg1.complete) ctx.drawImage(bg1,ds,0        ,width,bg1.height * width / bg1.width);
+    if(bg2.complete) ctx.drawImage(bg2,ds + width,0,width,bg2.height * width / bg2.width);
+  }
+  animateBg(){
+    this.x -= this.speed * this.gameSpeed;
+  }
+  setSpeed(newspeed) {
+    this.speed = newspeed;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,48 +1,90 @@
 let canvas,ctx;
-
-let SPEED = 2;
-
-
-let pepeImage;
-let pepeIndex = 0;
-pepeStateImages = pepeIdleImages;
-pepeImage = pepeStateImages[pepeIndex];
+let bgScale = 0.5;
+let gameSpeed = 1;
+let clouds, mounts1,mounts2,mounts3;
+let Pepe, Tito, Pedro, Vito, Diego, Carlos;
+let isPause = true;
 
 
 function init(){
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
 
-	//animateCharacter(Pepe);
-	setInterval(function(){
-		document.getElementById("img").src = Pepe.image.src;
-	},50);
+	mounts1 = new BgImage("img/5.BG/Pics/1.Mounts",canvas,1);
+	mounts2 = new BgImage("img/5.BG/Pics/2.Mounts",canvas,0.5);
+	mounts3 = new BgImage("img/5.BG/Pics/3.Mounts",canvas,0.2);
+	clouds  = new BgImage("img/5.BG/Pics/4.Clouds",canvas,0.3);
+	clouds.gameSpeed = 0.2;
+
+
+	Pepe = new Character(100,240,canvas,0.2);
+	Pepe.addState('Idle',"img/2.Pepe/1.Idle/Idle",10);
+	Pepe.addState('Long_Idle',"img/2.Pepe/1.Idle/Long_Idle",10);
+	Pepe.addState('Walk',"img/2.Pepe/2.Walk",6);
+	Pepe.addState('Jump',"img/2.Pepe/3.Jump",9);
+	Pepe.addState('Death',"img/2.Pepe/5.Death",7);
+	Pepe.addState('Death',"img/2.Pepe/5.Death",7);
+	Pepe.setState('Idle');
+/*
+	Tito = new Character(400,330,canvas,0.1);
+	Tito.addState('Idle',"img/2.Pepe/1.Idle/Idle",10);
+	Tito.addState('Long_Idle',"img/2.Pepe/1.Idle/Long_Idle",10);
+	Tito.addState('Walk',"img/2.Pepe/2.Walk",6);
+	Tito.addState('Jump',"img/2.Pepe/3.Jump",9);
+	Tito.addState('Death',"img/2.Pepe/5.Death",7);
+	Tito.addState('Death',"img/2.Pepe/5.Death",7);
+	Tito.setState('Idle');
+
+	Pedro = new Character(620,350,canvas,0.07);
+	Pedro.addState('Idle',"img/2.Pepe/1.Idle/Idle",10);
+	Pedro.addState('Long_Idle',"img/2.Pepe/1.Idle/Long_Idle",10);
+	Pedro.addState('Walk',"img/2.Pepe/2.Walk",6);
+	Pedro.addState('Jump',"img/2.Pepe/3.Jump",9);
+	Pedro.addState('Death',"img/2.Pepe/5.Death",7);
+	Pedro.addState('Death',"img/2.Pepe/5.Death",7);
+	Pedro.setState('Idle');
+*/
 	draw();
 	listenForKeys();
 }
 
 function draw(){
 	drawBackground();
-	updateCharacter(Pepe);
+	Pepe.drawImage();
 	requestAnimationFrame(draw);
 }
 
 function drawBackground(){
-	ctx.fillStyle = "white";
-	ctx.fillRect(0,0,canvas.width,canvas.height);
+	let bg = new Image();  						// warum nicht einmal definieren und dann nur drawImage...?
+	bg.src = "img/5.BG/Pics/5.Horizont.png";
+	if(bg.complete) ctx.drawImage(bg,0,0,canvas.width,canvas.height);
 	drawGround();
 }
 
 function drawGround(){
-	ctx.fillStyle = "yellow";
-	ctx.fillRect(0,100,canvas.width,canvas.height);
+	clouds.drawImage();
+	mounts3.drawImage();
+	//Pedro.drawImage();
+	mounts2.drawImage();
+	//Tito.drawImage();
+	mounts1.drawImage();
+ctx.font = "30px Arial";
+ctx.fillText("Pepe: " + Pepe.x, 10, 50);
 }
 
-function updateCharacter(character){
-	let img = character.image;
-	//checkForJumping();
-	ctx.drawImage(img,character.x,character.y,img.width*character.scale,img.height*character.scale);
+function Run(speed = 1){
+	mounts1.gameSpeed = speed;
+	mounts2.gameSpeed = speed;
+	mounts3.gameSpeed = speed;
+	Pepe.gameSpeed = speed;
 }
+function Stop(){
+	mounts1.gameSpeed = 0;	
+	mounts2.gameSpeed = 0;	
+	mounts3.gameSpeed = 0;
+	Pepe.gameSpeed = 0;
+}
+
 /*
 function checkForJumping(){
 	if(isJumping){
@@ -65,25 +107,36 @@ function checkForJumping(){
 */
 
 function listenForKeys(){
-	let character = Pepe;
+
 	document.addEventListener("keydown", e => {
+		let character = Pepe;
 		const k = e.key;
+
+		console.log(e);
+		if( e.code == 'Space'){
+			if(isPause) {Run(gameSpeed); isPause = false;}
+			else {Stop(); isPause = true;}
+		}
 		if( k == 'ArrowRight'){
-			character.move();
+			if(!isPause) character.move();
 		}
 
 		if( k == 'ArrowUp'){
-			character.isJumping = true;
+			//character.isJumping = true;
 		}
 	});
 	document.addEventListener("keyup", e => {
+		let character = Pepe;
 		const k = e.key;
 		if( k == 'ArrowRight'){
 			character.halt();
 		}
 
 		if( k == 'ArrowUp'){
-			character.isJumping = false;
+			//character.isJumping = false;
 		}
+	});
+	document.addEventListener("mousedown", e => {
+		console.log(e.path);
 	});
 }
