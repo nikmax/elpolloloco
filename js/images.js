15ad72b1aacaf96ee7faa2d0b0fc31dec581afec
Anton Sander <mail@antonsander.de>
2021-04-07 17:44:41 +0200

    Update images.js

--- js/images.js
+++ js/images.js
@@ -1,18 +1,1 @@
 
-
-let pepeIdleImagesSrc =["img/2.Pepe/1.Idle/Idle","I-1.png", "I-2.png", "I-3.png", "I-4.png", "I-5.png", "I-6.png", "I-7.png", "I-8.png", "I-9.png", "I-10.png"];
-let pepeLongIdleSrc = [];
-let pepeWalkImagesSrc = ["img/2.Pepe/2.Walk","W-21.png","W-22.png","W-23.png","W-24.png","W-25.png","W-26.png"];
-let pepeJumpImagesSrc = ["img/2.Pepe/3.Jump","J-31.png","J-32.png","J-33.png","J-34.png","J-35.png","J-36.png","J-37.png","J-38.png","J-39.png","J-40.png"];
-let pepeStateImages = [];
-let pepeIdleImages = [];
-let pepeLongIdle = [];
-let pepeWalkImages = [];
-let pepeJumpImages = [];
-
-
-
-let BgImages1 = ["img/5.Fondo/Capas/1.suelo-fondo1/1.png", "img/5.Fondo/Capas/1.suelo-fondo1/2.png"];
-
-
-
